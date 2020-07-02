f0ba802f2a4de61ace8d6402141faface5372f97
Ben Rose <benrose207@gmail.com>
2020-07-02 11:15:42 -0700

    break out piece of hike show into sub-component

---
+++ frontend/components/hikes/hike_primary_content.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRoute, faExchangeAlt, faRetweet, faMountain } from "@fortawesome/free-solid-svg-icons";
+
+const HikePrimaryContent = ({hike, tags}) => {
+
+    const features = tags.filter(tag => tag.tagType === "feature");
+    const feature = features[Math.floor(Math.random() * features.length)].name;
+    const featureDescription = (feature[feature.length - 1] === "s" || feature === "wildlife" ?
+        feature : `a ${feature}`);
+
+    const tagCloud = tags.map(tag => (
+        <h4 key={tag.id} className="tag">{tag.name}</h4>
+    ));
+
+    return (
+        <>
+            <p className="hike-summary">
+                {hike.name} is a {hike.distance} mile {hike.usage === "heavy" ? "heavily" : hike.usage + "ly"} trafficked {hike.routeType.toLowerCase()} trail located near Yosemite Valley, California that features {featureDescription} and is rated as {hike.difficulty}. The trail offers a number of activity options.
+                                </p>
+            <section className="hike-stats">
+                <div className="stat">
+                    <FontAwesomeIcon icon={faRoute} />
+                    <div>
+                        <span>Distance:</span>
+                        <span>{hike.distance} miles</span>
+                    </div>
+                </div>
+                {hike.elevationGain ?
+                    <div className="stat">
+                        <FontAwesomeIcon icon={faMountain} />
+                        <div>
+                            <span>Elevation Gain:</span>
+                            <span>{hike.elevationGain} feet</span>
+                        </div>
+                    </div> : null
+                }
+                <div className="stat">
+                    <FontAwesomeIcon icon={hike.routeType === "Loop" ?
+                        faExchangeAlt : faRetweet} />
+                    <div>
+                        <span>Route Type:</span>
+                        <span>{hike.routeType}</span>
+                    </div>
+                </div>
+            </section>
+            <section className="tag-cloud">
+                {tagCloud}
+            </section>
+        </>
+    )
+}
+
+export default HikePrimaryContent;
--- frontend/components/hikes/hike_show.jsx
+++ frontend/components/hikes/hike_show.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRoute, faExchangeAlt, faRetweet, faMountain, faExpandArrowsAlt, faMapSigns } from "@fortawesome/free-solid-svg-icons";
+import { faExpandArrowsAlt, faMapSigns } from "@fortawesome/free-solid-svg-icons";
+import HikePrimaryContent from "./hike_primary_content";
 import HikeMap from "../maps/hike_map";
 import CreateReviewContainer from "../reviews/create_review_container";
 import ReviewFeed from "../reviews/review_feed";
@@ -71,15 +72,6 @@ class HikeShow extends React.Component {
             const starClass = avgRating >= i ? " filled" : "";
             reviewStars.push(<span key={i} className={`star${starClass}`}></span>)
         }
-
-        const tagCloud = tags.map(tag => (
-            <h4 key={tag.id} className="tag">{tag.name}</h4>
-        ));
-        
-        const features = tags.filter(tag => tag.tagType === "feature");
-        const feature = features[Math.floor(Math.random() * features.length)].name;
-        const featureDescription = (feature[feature.length - 1] === "s" || feature === "wildlife" ? 
-            feature : `a ${feature}`);
 
         const contentTabs = (
             <>
@@ -175,38 +167,7 @@ class HikeShow extends React.Component {
                     </div>
                     <div className="hike-main">
                         <article className="hike-main-content">
-                            <p className="hike-summary">
-                                {hike.name} is a {hike.distance} mile {hike.usage === "heavy" ? "heavily" : hike.usage + "ly"} trafficked {hike.routeType.toLowerCase()} trail located near Yosemite Valley, California that features {featureDescription} and is rated as {hike.difficulty}. The trail offers a number of activity options.
-                            </p>
-                            <section className="hike-stats">
-                                <div className="stat">
-                                    <FontAwesomeIcon icon={faRoute} />
-                                    <div>
-                                        <span>Distance:</span>
-                                        <span>{hike.distance} miles</span>
-                                    </div>
-                                </div>
-                                {hike.elevationGain ? 
-                                    <div className="stat">
-                                        <FontAwesomeIcon icon={faMountain} />
-                                        <div>
-                                            <span>Elevation Gain:</span>
-                                            <span>{hike.elevationGain} feet</span>
-                                        </div>
-                                    </div> : null
-                                }
-                                <div className="stat">
-                                    <FontAwesomeIcon icon={hike.routeType === "Loop" ?
-                                        faExchangeAlt : faRetweet} />
-                                    <div>
-                                        <span>Route Type:</span>
-                                        <span>{hike.routeType}</span>
-                                    </div>
-                                </div>
-                            </section>
-                            <section className="tag-cloud">
-                                {tagCloud}
-                            </section>
+                            <HikePrimaryContent hike={hike} tags={tags}/>
                             <section className="hike-content">
                                 {contentTabs}
                             </section>
