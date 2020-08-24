f2e6852ad06bc478bf743ca90d22e3133fb2b3ee
Ben Rose <benrose207@gmail.com>
2020-08-24 16:42:40 -0700

    footer link adjustments

--- frontend/components/nav/footer.jsx
+++ frontend/components/nav/footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMountain } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub, faAngellist } from "@fortawesome/free-brands-svg-icons";
+import ReactGA from "react-ga";
 
 const Footer = (props) => {
     return (
@@ -12,11 +13,26 @@ const Footer = (props) => {
                     <h3>AllHikes</h3>
                 </div>
                 <div className="footer-links">
-                    <a href="https://www.linkedin.com/in/ben-rose-483549124/" target="_blank">
+                    <a
+                        href="https://www.linkedin.com/in/benjamin-rose-483549124/"
+                        target="_blank"
+                        onClick={() => ReactGA.event({category: 'click', action: "linkedin", label: 'footer'})}
+                    >
                         <FontAwesomeIcon icon={faLinkedin} />
                     </a>
-                    <a href="https://github.com/benrose207" target="_blank">
+                    <a
+                        href="https://github.com/benrose207"
+                        target="_blank"
+                        onClick={() => ReactGA.event({ category: 'click', action: "github", label: 'footer' })}
+                    >
                         <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a
+                        href="https://angel.co/benjamin-rose-2"
+                        target="_blank"
+                        onClick={() => ReactGA.event({ category: 'click', action: "angellist", label: 'footer' })}
+                    >
+                        <FontAwesomeIcon icon={faAngellist} />
                     </a>
                 </div>
             </div>
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./app";
+import ReactGA from "react-ga";
+
 
 const Root = ({ store }) => {
+    
+    ReactGA.initialize('UA-175985833-3');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
     return(
         <Provider store={store}>
             <HashRouter>
