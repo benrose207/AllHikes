db1745a82dc67adcd4e2802094881abd0cb0349f
Ben Rose <benrose207@gmail.com>
2020-08-24 17:00:06 -0700

    adding basic google analytics tagging and events

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
 import App from "./app";
 import ReactGA from "react-ga";
 
 
 const Root = ({ store }) => {
-    
+    const history = createHistory();
     ReactGA.initialize('UA-175985833-3');
     ReactGA.pageview(window.location.pathname + window.location.search);
+    history.listen((location) => {
+        ReactGA.pageview(location.pathname + location.search);
+    });
 
     return(
         <Provider store={store}>
-            <HashRouter>
+            <HashRouter history={history}>
                 <App />
             </HashRouter>
         </Provider>
