c7245e846524cab88cccda0ee06c8759c4f7fb3b
Ben Rose <benrose207@gmail.com>
2020-08-24 19:56:45 -0700

    fix pageview tracking for google analytics

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory } from "history";
 import App from "./app";
 import ReactGA from "react-ga";
 
 
 const Root = ({ store }) => {
-    const history = createHistory();
+    const history = createBrowserHistory();
     ReactGA.initialize('UA-175985833-3');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.pageview(window.location.pathname + location.hash.slice(1) + window.location.search);
     history.listen((location) => {
-        ReactGA.pageview(location.pathname + location.search);
+        ReactGA.pageview(location.pathname + location.hash.slice(1) + location.search);
     });
 
     return(
         <Provider store={store}>
-            <HashRouter history={history}>
+            <HashRouter>
                 <App />
             </HashRouter>
         </Provider>
