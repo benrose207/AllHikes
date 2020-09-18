d07458f9d1d95ba2b74b11f4131d91ed229a9b51
Ben Rose <benrose207@gmail.com>
2020-09-17 17:45:41 -0700

    add react-axe for dev accessibility testing

--- frontend/all_hikes.jsx
+++ frontend/all_hikes.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import configureStore from "./store/store";
-import Root from "./components/root";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root.jsx';
 
-document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
+document.addEventListener('DOMContentLoaded', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const axe = require('react-axe');
+    axe(React, ReactDOM, 1000);
+  }
 
-    let store;
-    if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: { [window.currentUser.id]: window.currentUser }
-            },
-            session: { currentUserId: window.currentUser.id }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
+  const root = document.getElementById('root');
 
-    ReactDOM.render(<Root store={store} />, root);
-})
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser },
+      },
+      session: { currentUserId: window.currentUser.id },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  ReactDOM.render(<Root store={store} />, root);
+});
